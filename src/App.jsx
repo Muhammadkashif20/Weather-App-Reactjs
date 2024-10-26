bec05c212cfbc146de5a2391a18973a63e45e420
muhammadkashif20 <kashif28@gmail.com>
2024-10-26 12:45:25 +0500

    finally Completed My Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@
   function App() {
     const [choseCity, setChoseCity] = useState(Allcities[0]);
     const [weatherData, setWeatherData] = useState(null);
+    const [loading,setLoading]=useState(false)
     //  1st Function:-
     const handleCityChange = (e) => {
       setChoseCity(e.target.value);
     };
     console.log("choseCity=>", choseCity);
     useEffect(() => {
+      setLoading(true)
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${choseCity}&appid=9cd5806461ac968d83fa18eb58801936`
       )
@@ -20,6 +22,7 @@
           res;
           console.log(res);
           setWeatherData(res);
+          setLoading(false)
         });
     }, [choseCity]);
     // WeatherOverview:
@@ -36,11 +39,9 @@
     const pressure=weatherData?.main?.pressure
     const speed=Math.round(weatherData?.wind?.speed)
     const sunrise = Math.round(weatherData?.sys?.sunrise);
-const sunset = Math.round(weatherData?.sys?.sunset);
+    const sunset = Math.round(weatherData?.sys?.sunset);
     const sunriseTime=new Date(sunrise*1000).toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
     const sunsetTime=new Date(sunset*1000).toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
-    
-
     return (
       <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-800 to-gray-900 text-white">
         <div className="w-full max-w-sm p-6 bg-gray-800 bg-opacity-90 rounded-lg shadow-lg">
@@ -60,10 +61,18 @@ const sunset = Math.round(weatherData?.sys?.sunset);
               })}
             </select>
           </div>
+          {loading?(
+            <div className="flex justify-center">
+            <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div> 
+          </div>)
+          :(
+            <>
           <WeatherOverView feels_like={feels_like} weather={weather} temp={temp} urlIcon={urlIcon}/>
           <WeatherDetails humidity={humidity} day_Time={day_Time} pressure={pressure} speed={speed} sunrise={sunriseTime} sunset={sunsetTime}/>
-        </div>
-      </div>
+          </>
+        )}
+          </div>
+          </div>
     );
   }
 
